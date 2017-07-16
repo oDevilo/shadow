af5a44ebc2c49e683dfd7fee6f16265d0849a63e
oDevilo <15757166470@163.com>
2017-07-16 22:25:56 +0800

    重新构思

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 route 接口,用于分库
 
-实现方案：读写分离
+实现方案：读写分离，db还是在mybatis-config配置
+shadowconfig配置文件如下
+
+1.<dbStrategy>一个类，配置db的分库策略，配置db的list
+2.<tableStrategy>多个表的分表策略，配置对应的table的list
 
 分表
---
+++ src/main/java/org/devil/myshadow/TagConstants.java
@@ -0,0 +1,15 @@
+package org.devil.myshadow;
+
+public interface TagConstants {
+    public static final String dbServer = "dbServer";
+
+    interface DbServer {
+        public static final String name = "name";
+        public static final String type = "name";
+        String driver = "driver";
+        public static final String url = "url";
+        public static final String username = "username";
+        public static final String password = "password";
+    }
+
+}
